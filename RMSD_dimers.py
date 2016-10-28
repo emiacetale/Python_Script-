2bccfc7ebbcc910b66b13986827d59ed33525e03
emiacetale <emiliano.brini@stonybrook.edu>
2016-10-28 15:20:43 -0400

    modified:   RMSD_dimers.py

--- RMSD_dimers.py
+++ RMSD_dimers.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python  
- 
+
+import copy as copy
 import argparse
 import mdtraj as md 
-
+import cPickle as pickle 
 
 # f.write('{:5s}   {:8.6f}\n'.format(el[0],el[1]))
 
@@ -10,19 +11,50 @@ def get_trjs(trj_files,top='XXX'):
     trajs = []
     for el in trj_files:
         if top != 'XXX':
-           trajs.append(md.load(el,top=top))
+           tmp=md.load(el,top=top)
         else:
-           trajs.append(md.load(el))
+           tmp=md.load(el)
+        #We keep only BB coordinates
+        ai=tmp.top.select("backbone")
+        tmp.atom_slice(ai,inplace=True)
+        trajs.append(copy.deepcopy(tmp))
     return trajs
 
+def get_RMSDs(trajs,refs):
+    RMSDs=[]
+    for traj in trajs:
+        RMSD=[]
+        for ref in refs:
+            print ref.top.select("chainid 0"), traj.top.select("chainid 0")
+            #We allign the 1st chain                                             
+            #traj.superpose(ref,frame=0,atom_indices=traj.top.select("chainid 0"),ref_atom_indices=ref.top.select("chainid 0"))
+            traj.superpose(ref,frame=0,atom_indices=ref.top.select("chainid 0"),ref_atom_indices=ref.top.select("chainid 0"))
+            #And we compute the RMSD of the 2nd chain
+            #rmsd=md.rmsd(traj,ref,frame=0,atom_indices=traj.top.select("chainid 1"), 
+            #              ref_atom_indices=ref.top.select("chainid 1"),precentered=True)
+            #rmsd=md.rmsd(traj,ref,frame=0,atom_indices=ref.top.select("chainid 1"),
+            #              ref_atom_indices=ref.top.select("chainid 1"),precentered=True)
+            rmsd=calc_RMDS(trj.atom_slice(traj.top.select("chainid 1")),ref.atom_slice(traj.top.select("chainid 1")))
+            RMSD.append(rmsd)
+        RMSDs.append(RMSD)
+    return RMSDs
 
-
+def calc_RMDS(trj,ref):
+    RMSDs=[]
+    coord_trj=trj.xyz
+    coord_ref=tef.xyz[0]
+    for coord in coord_trj:
+        dist=np.square(coord-coord_trj)
+        rmsd=np.sqrt((np.sum(dist))/len(dist))
+        RMSDs.append(rmsd)
+    return RMSDs
 
 def parse_args():                              #in line argument parser with help 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-top',help='topology files', nargs=1)
+    parser.add_argument('-top',help='topology files')
     parser.add_argument('-trajs', help='files to read', nargs='+')
     parser.add_argument('-refpdbs', help='value to consider',nargs='+')
+    parser.add_argument('-out', help='output pickl file name',default='RMSD_bu.pckl',nargs=1)
     #parser.add_argument('-gn', help='number identifier of the group to highlight', default='428')
     return parser.parse_args()
 
@@ -30,13 +62,12 @@ def main():
     args = parse_args()       
     trajs = get_trjs(args.trajs,args.top) 
     refs  = get_trjs(args.refpdbs)
-    print trajs
-    print refs
+    RMSDs = get_RMSDs(trajs,refs)
+    with open(args.out[0],'w') as BUF:
+         pickle.dump(RMSDs,BUF)
 
-    #result=files_read_and_sort(args.f,args.val) #result=[{gr1:{q1:val1_g1, q2:val2_g1, ... }, gr2:{...}}_file1, {}_file_2,...]
-    #av_results=average(result)
-    #plot_results(av_results, args.gn)
-    #print_results(av_results,'ranking')
+
+
 
 if __name__ == '__main__': #Weird Python way to execute main()
     main()
